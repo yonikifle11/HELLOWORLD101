ff558a24cf8b385672b9ddf7d58c18392ec0670b
yonikifle11 <107622693+yonikifle11@users.noreply.github.com>
2022-06-25 19:41:22 +0300

    Add project files.

---
+++ HELLOWORLD/Program.cs
@@ -0,0 +1,14 @@
+﻿// Hello World! program
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}

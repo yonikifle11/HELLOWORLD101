21d8330705c05939b33f5e9f26b3c4bc007078f2
yonikifle11 <107622693+yonikifle11@users.noreply.github.com>
2022-06-25 23:22:58 +0300

    evenodd

--- HELLOWORLD/Program.cs
+++ HELLOWORLD/Program.cs
@@ -1,13 +1,32 @@
-﻿// Hello World! program
+﻿//even odd program
 using System;
 
-namespace HelloWorld
+namespace evenorodd
 {
-    class Hello
+    class parity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            char choice;
+            do
+            {
+                Console.WriteLine("enter the number");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                if (num % 2 == 0)
+                    Console.WriteLine("the number is even");
+                else
+                    Console.WriteLine("the number is odd");
+
+                Console.WriteLine("do u want to continue Y or N");
+
+                choice = Console.ReadLine()[0];
+            }
+            while (choice != 'N' && choice != 'n');
+
         }
 
     }
